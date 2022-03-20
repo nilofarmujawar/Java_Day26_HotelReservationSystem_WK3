55816ade6a9f3dfc37a037f5f1ce3f203d2f61d6
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-20 15:52:54 +0530

    Added: Hotel with price for regular customer functionality added UC1.

---
+++ src/test/java/com/workshop3/HotelReservationSystemTest.java
@@ -0,0 +1,22 @@
+package com.workshop3;
+
+/**
+ * import Assertions class
+ * import test class
+ */
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * creating a class name as HotelReservationSystemTest
+ */
+public class HotelReservationSystemTest {
+
+    //uc1
+    @Test
+    public void givenHotelNamesWhenAddedShouldReturnSize() {
+        HotelReservationSystem obj = new HotelReservationSystem();
+        obj.addHotel();
+        Assertions.assertEquals(3, obj.hotelReservation.size());
+    }
+}
