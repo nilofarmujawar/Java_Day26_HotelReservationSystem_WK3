fe0a748b64be2c22c04c95c12b31ddb4258965b3
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-20 16:00:01 +0530

    Added: Cheapest hotel for regular customer functionality added UC2.

--- src/main/java/com/workshop3/Hotel.java
+++ src/main/java/com/workshop3/Hotel.java
@@ -23,6 +23,7 @@ class Hotel {
     int weekendRate;
     int specialWeekdayRate;
     int specialWeekendRate;
+    int avgRate = weekdayRate + weekendRate;
 
     /**
      * create a parameterized constructor
@@ -44,10 +45,25 @@ class Hotel {
         this.specialWeekdayRate = specialWeekdayRate;
         this.specialWeekendRate = specialWeekendRate;
     }
-/**
- * 1) The get method returns the variable value, and the set method sets the value.
- * 2) "this" keyword is used to refer to the current object.
- */
+    /**
+     * 1) The get method returns the variable value, and the set method sets the value.
+     * 2) "this" keyword is used to refer to the current object.
+     */
+
+    /**
+     * The getAvgRate method returns the value of the variable avgRate.
+     * @return avgRate
+     */
+    public int getAvgRate() {
+        return avgRate;
+    }
+    /**
+     * The ssetAvgRate method takes a parameter (avgRate) and assigns it to the avgRate variable
+     * @param avgRate taking in this Hotel reservation system.
+     */
+    public void setAvgRate(int avgRate) {
+        this.avgRate = avgRate;
+    }
 
     /**
      * The getHotelName method returns the value of the variable hotelName.
